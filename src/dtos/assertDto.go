4d8ed6edfda840b79926eca0b6b846d89f66ddae
mayke.furtado <mayke.furtado@compasso.com.br>
2022-08-22 11:16:04 -0300

    mapeando assert

--- src/dtos/assertDto.go
+++ src/dtos/assertDto.go
@@ -8,13 +8,29 @@ import (
 )
 
 type AssertDto struct {
-	Name string `json:"name"`
+	Id           uint32 `json:"id"`
+	Name         string `json:"name"`
+	Amount       int32 `json:"amount"`
+	Price        int64 `json:"price"`
+	AveragePrice int64 `json:"average_price"`
 }
 
 func (dto *AssertDto) Validate(action string) error {
 	switch strings.ToLower(action) {
 	case "update":
 		if dto.Name == "" {
+			return errors.New("Required name")
+		}
+		if dto.Amount != "" {
+			// verificar se nao e negativo
+			return errors.New("Required name")
+		}
+		if dto.Price != "" {
+			// verificar se nao e negativo
+			return errors.New("Required name")
+		}
+		if dto.AveragePrice != "" {
+			// verificar se nao e negativo
 			return errors.New("Required name")
 		}
 		return nil
@@ -30,5 +46,8 @@ func (dto *AssertDto) Validate(action string) error {
 func (dto *AssertDto) ToDomain() models.Assert {
 	return models.Assert{
 		Name: dto.Name,
+		Amount: dto.Amount,     
+		Price: dto.Price,     
+		AveragePrice: dto.AveragePrice,
 	}
 }
